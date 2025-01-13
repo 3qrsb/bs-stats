71793b7bb376782d36a46dade01aaa20f024dbfa
Yers <eabuov4@gmail.com>
2025-01-14 00:24:16 +0500

    forgotten setup :)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import HomePage from "./pages/HomePage";
 import PlayerInfoPage from "./pages/PlayerInfoPage";
 import Layout from "./components/layout/Layout";
@@ -8,44 +7,42 @@ import LeaderboardPage from "./pages/LeaderboardPage";
 
 const App = () => {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <HomePage />{" "}
-              </Layout>
-            }
-          />
-          <Route
-            path="/player/:tag"
-            element={
-              <Layout>
-                <PlayerInfoPage />{" "}
-              </Layout>
-            }
-          />
-          <Route
-            path="/leaderboard"
-            element={
-              <Layout>
-                <LeaderboardPage />{" "}
-              </Layout>
-            }
-          />
-          <Route
-            path="/leaderboard/global"
-            element={
-              <Layout>
-                <PlayerLeaderboardPage />{" "}
-              </Layout>
-            }
-          />
-        </Routes>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/player/:tag"
+          element={
+            <Layout>
+              <PlayerInfoPage />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/leaderboard"
+          element={
+            <Layout>
+              <LeaderboardPage />{" "}
+            </Layout>
+          }
+        />
+        <Route
+          path="/leaderboard/global"
+          element={
+            <Layout>
+              <PlayerLeaderboardPage />{" "}
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
