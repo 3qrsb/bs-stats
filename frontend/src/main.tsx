49dc91aa60824349b941f16129e7627e421f855f
Yers <eabuov4@gmail.com>
2025-03-21 23:24:31 +0500

    setting up sentry for logging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,12 @@
+import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
 import App from "./App.tsx";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
