4bff1793232a1a722c0ae1b8f64556c1725f98ab
Yers <eabuov4@gmail.com>
2025-04-10 01:10:19 +0500

    generating sitemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,22 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
-    org: "yersultan",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: "yersultan",
+      project: "javascript-react",
+    }),
+    sitemap({
+      hostname: "https://bs-stats.pages.dev",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
